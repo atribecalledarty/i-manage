8757f02fc6bce460f155f9b9b070fe1e5e41323a
atribecalledarty <arth3rs0ng@gmail.com>
2020-02-14 21:18:22 -0800

    added manager status property to a user

--- db/migrate/20200215045844_create_users.rb
+++ db/migrate/20200215045844_create_users.rb
@@ -1,11 +1,12 @@
 class CreateUsers < ActiveRecord::Migration[6.0]
   def change
     create_table :users do |t|
+      t.boolean :manager_status
       t.text :username
       t.text :email
       t.string :password_digest
       t.string :phone_number
-      
+
       t.timestamps
     end
   end
