61177d2e0e80937ca755e0ff724ed540882eb445
atribecalledarty <arth3rs0ng@gmail.com>
2020-02-14 21:10:12 -0800

    added properties to units migration and added hassecurepassword to user

--- db/migrate/20200215050548_create_units.rb
+++ db/migrate/20200215050548_create_units.rb
@@ -1,6 +1,9 @@
 class CreateUnits < ActiveRecord::Migration[6.0]
   def change
     create_table :units do |t|
+      t.integer :unit_number
+      t.integer :sq_ft
+      t.integer :number_of_bedrooms
 
       t.timestamps
     end
