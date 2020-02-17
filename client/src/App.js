d7f887aa888298307c13e5ae65606950210ac93d
atribecalledarty <arth3rs0ng@gmail.com>
2020-02-17 13:34:22 -0800

    added router and route to app js file

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      
-    </div>
+    <Router>
+      <div>
+        <Route exact path="/login" render={() => <Login />}/>
+      </div>
+    </Router>
   );
 }
 
