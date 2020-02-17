8b21e2da975da4dc63cd8713005b3e31073e20d6
atribecalledarty <arth3rs0ng@gmail.com>
2020-02-17 14:52:06 -0800

    added case statements to reducer

--- client/src/manageResources.js
+++ client/src/manageResources.js
@@ -1,6 +1,20 @@
 export default function manageResources (state = { units: [], users: [], loading: false }, action) {
 
     switch(action.type) {
+        case 'LOADING_UNITS':
+            return {
+                ...state,
+                units: [ ...state.units ],
+                users: [ ...state.users ],
+                loading: true
+            }
+        case 'ADDING_UNITS':
+            return {
+                ...state,
+                units: action.units,
+                users: [ ...state.users ],
+                loading: false
+            }
         default:
             return state;
     }
