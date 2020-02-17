6254799ce7e999b47e9b6324d283fb3b4ac60f8f
atribecalledarty <arth3rs0ng@gmail.com>
2020-02-17 15:31:08 -0800

    added first name and last name to users table

--- db/schema.rb
+++ db/schema.rb
@@ -33,6 +33,8 @@ ActiveRecord::Schema.define(version: 2020_02_15_051034) do
   create_table "users", force: :cascade do |t|
     t.boolean "manager_status"
     t.string "username"
+    t.string "first_name"
+    t.string "last_name"
     t.string "email"
     t.string "password_digest"
     t.string "phone_number"
