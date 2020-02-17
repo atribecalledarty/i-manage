9305a9a1619256d40dd5d1a372c7f2fea36a18c8
atribecalledarty <arth3rs0ng@gmail.com>
2020-02-17 14:58:47 -0800

    now redux thunk added, and also unitscontainer fetches units on mount and has access to units

--- client/src/UnitsContainer.js
+++ client/src/UnitsContainer.js
@@ -1,13 +1,32 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { fetchUnits } from './unitsAction';
 
 class UnitsContainer extends Component {
+    componentDidMount(){
+        this.props.fetchUnits();
+    }
+
     render() {
         return(
             <div>
                 I am UnitsContainer component!
+                {console.log(this.props.units)}
             </div>
         )
     }
 }
 
-export default UnitsContainer;
+const mapDispatchToProps = dispatch => {
+    return {
+        fetchUnits: () => dispatch(fetchUnits())
+    }
+}
+
+const mapStateToProps = state => {
+    return {
+        units: state.units
+    }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(UnitsContainer);
