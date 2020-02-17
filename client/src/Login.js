fb67e8fd842e909d876b947424b8897bd925ad29
atribecalledarty <arth3rs0ng@gmail.com>
2020-02-17 13:46:03 -0800

    created basic form for login page

--- client/src/Login.js
+++ client/src/Login.js
@@ -1,10 +1,25 @@
 import React, { Component } from 'react';
 
 class Login extends Component {
+    state = {
+        email: '',
+        password: ''
+    }
+
     render() {
         return(
             <div>
-                Hi
+                <form>
+                    <label for="email">Email </label>
+                    <input type="text" name="email" id="email" />
+                    <br />
+                    
+                    <label for="password">Password </label>
+                    <input type="password" name="password" id="password" />
+                    <br/>
+
+                    <input type="submit" value="Sign In"/>
+                </form>
             </div>
         )
     }
