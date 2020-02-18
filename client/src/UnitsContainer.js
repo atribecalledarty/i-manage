9af7230b66abd52f49dbe19052e853be5c8f0073
atribecalledarty <arth3rs0ng@gmail.com>
2020-02-18 15:55:52 -0800

    removed route and show component for user; I want app to be simplistic, so clicking a unit will display everything about the residency and I will have different page for users list

--- client/src/UnitsContainer.js
+++ client/src/UnitsContainer.js
@@ -3,7 +3,6 @@ import { connect } from 'react-redux';
 import { fetchUnits } from './unitsAction';
 import UnitsList from './UnitsList';
 import UnitShow from './UnitShow';
-import UserShow from './UserShow';
 import { Route } from 'react-router-dom';
 
 class UnitsContainer extends Component {
@@ -18,7 +17,6 @@ class UnitsContainer extends Component {
                 {/* {console.log(this.props.units)} */}
                 <UnitsList units={this.props.units} />
                 <Route exact path={`${this.props.match.url}/:unitId`} component={UnitShow} />
-                <Route exact path={`${this.props.match.url}/users/:userId`} component={UserShow} />
             </div>
         )
     }
