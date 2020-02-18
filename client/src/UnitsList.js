4e77ddefa432680a03aaeb6d3dabbe30ffbec2c3
atribecalledarty <arth3rs0ng@gmail.com>
2020-02-18 15:57:28 -0800

    removed link for user

--- client/src/UnitsList.js
+++ client/src/UnitsList.js
@@ -6,12 +6,13 @@ const UnitsList = ({ units }) => {
     const renderUnits = units.map(unit =>
         <div> 
             <Link key={unit.id} to={`/units/${unit.id}`}>Unit {unit.unit_number}</Link>  
-            &nbsp;<Link 
+            &nbsp;{unit.resident.first_name} {unit.resident.last_name}
+            {/* <Link 
                 key={unit.resident.id} 
                 to={`/units/users/${unit.resident.id}`}
             >
                     {unit.resident.first_name} {unit.resident.last_name}
-            </Link>
+            </Link> */}
         </div>
     );
     
