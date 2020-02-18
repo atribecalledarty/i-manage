df90713f388af1446c3f22c57caf79468e75322f
atribecalledarty <arth3rs0ng@gmail.com>
2020-02-18 15:42:44 -0800

    now nested routes and links to each unit works properly

--- client/src/UnitsList.js
+++ client/src/UnitsList.js
@@ -1,13 +1,17 @@
-import React, { Component } from 'react';
-// import Unit from './Unit';
+import React from 'react';
+import { Link } from 'react-router-dom';
 
 const UnitsList = ({ units }) => {
-    const renderUnits = Object.keys(units).map(unitId => 
-        <Link key={unitId} to={`/movies/${unitId}`}>{units[unitId].unit_number}</Link>
+    const renderUnits = units.map(unit =>
+        <div> 
+            <Link key={unit.id} to={`/movies/${unit.id}`}>{unit.unit_number}</Link> - 
+            
+        </div>
     );
     
     return (
         <div>
+            I am UnitsList component!
             {renderUnits}
         </div>
     )
