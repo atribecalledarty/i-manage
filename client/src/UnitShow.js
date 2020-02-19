36678df4b75989f499ec0da7053b0a2de0ef35df
atribecalledarty <arth3rs0ng@gmail.com>
2020-02-18 17:18:25 -0800

    adding properties to unit show page

--- client/src/UnitShow.js
+++ client/src/UnitShow.js
@@ -1,27 +1,17 @@
 import React from 'react';
 
-// class UnitShow extends React.Component {
-//     render() {
-//         return(
-//             <div>
-//                 {/* {console.log(this.props.unit, "first console log")} */}
-//                 {/* {console.log(this.props.unit.id)} */}
-//                 <h2>{this.props.unit.id}</h2>
-//             </div>
-//         )
-//     }
-// }
-
 const UnitShow = ({ match, units }) => {
     const unit = units.find(unit => unit.id == match.params.unitId)
 
     return (
         <div>
-            {/* {console.log(match.params.unitId)}
-            {console.log(units[0].id)}
-            {console.log(units.find(unit => unit.id == match.params.unitId))} */}
+            {console.log(unit)}
             <h3>Unit {unit.unit_number}</h3>
-            <p></p>
+            <p>
+                {unit.type_of_unit}<br/>
+                {unit.sq_ft} sq ft<br/>
+                ${unit.rent_cost_per_month}/month
+            </p>
 
         </div>  
     )
