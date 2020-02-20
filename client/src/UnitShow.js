d179d5d139ac712428ee78e61e62e1fc19f195c0
atribecalledarty <arth3rs0ng@gmail.com>
2020-02-19 22:15:21 -0800

    unit show page now displays resident info

--- client/src/UnitShow.js
+++ client/src/UnitShow.js
@@ -2,6 +2,7 @@ import React from 'react';
 
 const UnitShow = ({ match, units }) => {
     const unit = units.find(unit => unit.id == match.params.unitId)
+    const resident = unit.resident
 
     return (
         <div>
@@ -11,6 +12,12 @@ const UnitShow = ({ match, units }) => {
                 {unit.type_of_unit}<br/>
                 {unit.sq_ft} sq ft<br/>
                 ${unit.rent_cost_per_month}/month
+            </p>
+
+            <p>
+                {resident.first_name} {resident.last_name}<br/>
+                {resident.email}<br/>
+                {resident.phone_number}
             </p>
 
         </div>  
