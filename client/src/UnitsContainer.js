2c3cf26e1f123a1fa9547b5ce9d30af0309b2cf7
atribecalledarty <arth3rs0ng@gmail.com>
2020-02-22 20:57:35 -0800

    Took loading units out of UnitsContainer, and changed to functional component

--- client/src/UnitsContainer.js
+++ client/src/UnitsContainer.js
@@ -1,38 +1,18 @@
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
-import { fetchUnits } from './fetchActions';
+import React from 'react';
 import UnitsList from './UnitsList';
 import UnitShow from './UnitShow';
 import { Route } from 'react-router-dom';
 
-class UnitsContainer extends Component {
-    componentDidMount(){
-        this.props.fetchUnits();
-    }
-
-    render() {
-        return(
-            <div>
-                {/* I am UnitsContainer component! */}
-                {/* {console.log(this.props.units)} */}
-                <UnitsList units={this.props.units} />
-                <Route path={`${this.props.match.url}/:unitId`} 
-                    render={routerProps => <UnitShow {...routerProps} units={this.props.units}/>}/>
-            </div>
-        )
-    }
+const UnitsContainer = ({ match, units }) => {
+    return(
+        <div>
+            {/* I am UnitsContainer component! */}
+            {/* {console.log(this.props.units)} */}
+            <UnitsList units={units} />
+            <Route path={`${match.url}/:unitId`} 
+                render={routerProps => <UnitShow {...routerProps} units={units}/>}/>
+        </div>
+    )
 }
 
-const mapDispatchToProps = dispatch => {
-    return {
-        fetchUnits: () => dispatch(fetchUnits())
-    }
-}
-
-const mapStateToProps = state => {
-    return {
-        units: state.units
-    }
-}
-
-export default connect(mapStateToProps, mapDispatchToProps)(UnitsContainer);
+export default UnitsContainer;
