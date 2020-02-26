16d446f9eb9bff5c1c8d8fa84f806c8e1187dd83
atribecalledarty <arth3rs0ng@gmail.com>
2020-02-26 05:43:49 -0800

    CreateUser component displays form with inputs when clicked

--- client/src/CreateUser.js
+++ client/src/CreateUser.js
@@ -2,7 +2,30 @@ import React from 'react';
 
 class CreateUser extends React.Component {
     displayUserInput = () => {
-        
+        return (
+            <form>
+                <label for="username">Username: </label>
+                <input type="text" id="username" name="username"/><br/>
+
+                <label for="username">Username: </label>
+                <input type="text" id="first_name" name="first_name"/><br/>
+
+                <label for="username">Username: </label>
+                <input type="text" id="last_name" name="last_name"/><br/>
+
+                <label for="username">Username: </label>
+                <input type="text" id="email" name="email"/><br/>
+
+                <label for="username">Username: </label>
+                <input type="text" id="username" name="username"/><br/>
+
+                <label for="username">Username: </label>
+                <input type="text" id="phone_number" name="phone_number"/><br/>
+                
+                <label for="username">Username: </label>
+                <input type="password" id="password" name="password"/><br/>
+            </form>
+        )
     }
 
     render() {
