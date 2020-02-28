18c9c68f5341adf28c1797f7f1ff08264469d921
atribecalledarty <arth3rs0ng@gmail.com>
2020-02-28 09:34:33 -0800

    Create action in userscontroller functioning

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,6 +7,16 @@ class UsersController < ApplicationController
     end
 
     def create
+        user = User.create(
+            username: params[:username],
+            first_name: params[:first_name],
+            last_name: params[:last_name],
+            email: params[:email],
+            phone_number: params[:phone_number],
+            password: params[:password]
+        )
+        users = User.all
+        render json: UserSerializer.new(users).to_serialized_json
     end
 
 
