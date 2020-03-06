1bf6a1390aeca0ec2231624543b54b54146acbbe
atribecalledarty <arth3rs0ng@gmail.com>
2020-03-06 05:56:48 -0800

    Generated Payment resource and migration

---
+++ db/migrate/20200306135513_create_payments.rb
@@ -0,0 +1,11 @@
+class CreatePayments < ActiveRecord::Migration[6.0]
+  def change
+    create_table :payments do |t|
+      t.date :transaction_date, :default => Date.today
+      t.integer :amount
+      t.integer :residency_id
+
+      t.timestamps
+    end
+  end
+end
