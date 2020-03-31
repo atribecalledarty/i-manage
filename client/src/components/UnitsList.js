a18aebbc3aa2f5d610b52b3fa5eeef042f262650
atribecalledarty <arth3rs0ng@gmail.com>
2020-03-30 17:18:21 -0700

    UnitsLists redirects properly onMount and Update, Also using eventKeys for different styling so I can have defaultActiveKey

--- client/src/components/UnitsList.js
+++ client/src/components/UnitsList.js
@@ -3,6 +3,18 @@ import { Link } from 'react-router-dom';
 import { ListGroup } from 'react-bootstrap';
 
 class UnitsList extends React.Component {
+    componentDidMount(){
+        if (this.props.units) {
+            this.props.history.push(`/units/${this.props.units[0].id}`)
+        }
+    }
+
+    componentDidUpdate(prevProps){ //need both because initial loading of units will not have units loaded into props
+        if (prevProps.units !== this.props.units) {
+            this.props.history.push(`/units/${this.props.units[0].id}`)
+        }
+    }
+
     renderUser = unit => {
         if (unit.resident !== undefined) {
             return (
@@ -23,7 +35,8 @@ class UnitsList extends React.Component {
         return (
             this.props.units.map(unit =>
                 <div key={unit.id}> 
-                    <ListGroup.Item action onClick={() => this.clickHandler(unit)}>#{unit.unit_number} {this.renderUser(unit)}</ListGroup.Item> 
+                    {/* <ListGroup.Item action onClick={() => this.clickHandler(unit)}>#{unit.unit_number} {this.renderUser(unit)}</ListGroup.Item>  */}
+                    <ListGroup.Item action eventKey={`/units/${unit.id}`}>#{unit.unit_number} {this.renderUser(unit)}</ListGroup.Item> 
                 </div>
             )
         )
@@ -33,7 +46,7 @@ class UnitsList extends React.Component {
     
     render() {
         return (
-            <ListGroup>
+            <ListGroup defaultActiveKey={`/units/${this.props.units[0].id}`} onSelect={selectedKey => this.props.history.push(selectedKey)}>
                 {this.renderUnits()}
             </ListGroup>
         )
