43b9e536a126ca30794191ac600092d2534e537b
atribecalledarty <arth3rs0ng@gmail.com>
2020-04-03 13:22:34 -0700

    Now UnitShow will not render if units have not been loaded, This was causing an error when you try to navigate to /units/:unitId

--- client/src/components/UnitShow.js
+++ client/src/components/UnitShow.js
@@ -8,18 +8,17 @@ import NewResidencyForm from './NewResidencyForm';
 
 const UnitShow = ({ match, units, deleteResidency, addResidency, usersWithoutResidency }) => {
     const unit = units.find(unit => unit.id === Number(match.params.unitId))
-    const resident = unit.resident;
-    const residency = unit.residency;
+
 
     const renderResidencyInfo = () => {
         if (unit.residency !== undefined) {
             return (
                 <div>
                     <p>
-                        <span id="resident-name"><Link to={`/users/${resident.id}`}>{resident.first_name} {resident.last_name}</Link></span><br/>
-                        Resident Since {returnFormattedDate(residency.start_date)}<br/>
-                        {resident.email} | {resident.phone_number}<br/><br/>
-                        <Button size="sm" variant="outline-danger" onClick={() => deleteResidency(residency.id)}>Remove Resident From Unit</Button>
+                        <span id="resident-name"><Link to={`/users/${unit.resident.id}`}>{unit.resident.first_name} {unit.resident.last_name}</Link></span><br/>
+                        Resident Since {returnFormattedDate(unit.residency.start_date)}<br/>
+                        {unit.resident.email} | {unit.resident.phone_number}<br/><br/>
+                        <Button size="sm" variant="outline-danger" onClick={() => deleteResidency(unit.residency.id)}>Remove Resident From Unit</Button>
                     </p>
                 </div>
             )
@@ -30,16 +29,26 @@ const UnitShow = ({ match, units, deleteResidency, addResidency, usersWithoutRes
         }
     }
 
+    const renderUnitInfo = () => {
+        if (unit !== undefined) {
+            return (
+                <>
+                    <h3>Unit {unit.unit_number}</h3>
+                    <p>
+                        <i>{unit.type_of_unit}<br/>
+                        {unit.sq_ft} sq ft<br/>
+                        ${unit.rent_cost_per_month}/month</i>
+                    </p>
+                    {renderResidencyInfo()}
+                </>
+            )
+        }
+    }
+
     return (
         <div id="unit-show">
-            {console.log('in unitshow', units)}
-            <h3>Unit {unit.unit_number}</h3>
-            <p>
-                <i>{unit.type_of_unit}<br/>
-                {unit.sq_ft} sq ft<br/>
-                ${unit.rent_cost_per_month}/month</i>
-            </p>
-            {renderResidencyInfo()}
+            {/* {console.log('in unitshow', units)} */}
+            {renderUnitInfo()}
             
             <Route path={`/units/:unitId/residents/new`} render={routerProps => 
                     <NewResidencyForm 
--- client/src/components/UnitsContainer.js
+++ client/src/components/UnitsContainer.js
@@ -5,19 +5,6 @@ import { Route } from 'react-router-dom';
 import { Jumbotron, Col, Row } from 'react-bootstrap';
 
 class UnitsContainer extends React.Component {
-    // componentDidMount(){ //need this one when users have been loaded already, and onmount we need to redirect
-    //     // eslint-disable-next-line
-    //     if (!this.props.loading_units && (JSON.stringify(this.props.units) != JSON.stringify([]))) {
-    //         this.props.history.push(`/units/${this.props.units[0].id}`)
-    //     }
-    // }
-
-    // componentDidUpdate(prevProps){
-    //     // eslint-disable-next-line
-    //     if (JSON.stringify(prevProps.units) != JSON.stringify(this.props.units)) {
-    //         this.props.history.push(`/units/${this.props.units[0].id}`)
-    //     }
-    // }
     componentDidMount(){ //need this one when units have been loaded already, and onmount we need to redirect
         if (this.props.units[0] !== undefined && this.props.history.location.pathname === '/units') {
             this.props.history.push(`/units/${this.props.units[0].id}`)
